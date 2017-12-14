fc842ad9730b5279741fb53c0191cee2d6afca90
perceptib1e <perceptib1e@mail.ru>
2017-12-14 14:42:31 +0200

    practice 4

--- src/symonenko/alexandr/Main.java
+++ src/symonenko/alexandr/Main.java
@@ -19,69 +19,9 @@ public class Main {
         Task task7 = new Task("Simple bound IN", 60);
         task7.setActive(true);
 
-        System.out.println(task1.equals(task3));
+        Task t1 = new Task("1",1);
+        Task t2 = new Task("2",2);
 
-        System.out.println(task1.hashCode());
-        System.out.println(task1.toString());
-        System.out.println(task6.toString());
-
-//        ArrayTaskList aTL = new ArrayTaskList();
-//        TaskList lTL = new LinkedTaskList();
-
-//        aTL.add(task1);
-//        aTL.add(task2);
-//        aTL.add(task3);
-//        aTL.add(task3);
-//        aTL.add(task5);
-//        aTL.add(task6);
-//
-//        lTL.add(task1);
-//        lTL.add(task2);
-//        lTL.add(task3);
-//        lTL.add(task4);
-//        lTL.add(task5);
-//        lTL.add(task6);
-
-        //ARRAY LIST TEST
-
-//        //checking all filled tasks in symonenko.alexandr.ArrayTaskList
-//        System.out.println("\n Length of FILLED symonenko.alexandr.ArrayTaskList = " + aTL.size() + "\n namely:");
-//        for (int i = 0; i < aTL.size; i++)
-//            System.out.println("in " + i + " cell = " + aTL.getTask(i).getTitle());
-//
-//        //checking ALL symonenko.alexandr.ArrayTaskList cells
-//        System.out.println("\n Length of ALL symonenko.alexandr.ArrayTaskList = " + aTL.arrayTaskList.length + "\n namely:");
-//        for (int i = 0; i < aTL.arrayTaskList.length; i++)
-//        System.out.println("in " + i + " cell = " + aTL.arrayTaskList[i]);
-//
-//        //removing task from symonenko.alexandr.ArrayTaskList
-//        System.out.println("\n Removing task7");
-//        aTL.remove(task7);
-//
-//        //checking all filled tasks in symonenko.alexandr.ArrayTaskList
-//        System.out.println("\n Length of FILLED symonenko.alexandr.ArrayTaskList = " + aTL.size() + "\n namely:");
-//        for (int i = 0; i < aTL.size; i++)
-//            System.out.println("in " + i + " cell = " + aTL.getTask(i).getTitle());
-//
-//        //checking ALL symonenko.alexandr.ArrayTaskList cells
-//        System.out.println("\n Length of ALL symonenko.alexandr.ArrayTaskList = " + aTL.arrayTaskList.length + "\n namely:");
-//        for (int i = 0; i < aTL.arrayTaskList.length; i++)
-//            System.out.println("in " + i + " cell = " + aTL.arrayTaskList[i]);
-
-//        //LINKED LIST TEST
-//
-//        //checking all tasks in symonenko.alexandr.LinkedTaskList
-//        System.out.println("\n Length of symonenko.alexandr.LinkedTaskList = " + lTL.size() + "\n namely:");
-//        for (int i = 0; i < lTL.size(); i++)
-//            System.out.println("in " + i + " cell = " + lTL.getTask(i).getTitle());
-//
-//        //removing task from symonenko.alexandr.LinkedTaskList
-//        System.out.println("\n Removing task7");
-//        lTL.remove(task7);
-//
-//        //checking all tasks in symonenko.alexandr.LinkedTaskList
-//        System.out.println("\n Length of symonenko.alexandr.LinkedTaskList = " + lTL.size() + "\n namely:");
-//        for (int i = 0; i < lTL.size(); i++)
-//            System.out.println("in " + i + " cell = " + lTL.getTask(i).getTitle());
+        System.out.println(t1.equals(t2));
     }
 }
